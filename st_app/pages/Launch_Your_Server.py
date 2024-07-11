5f2dc7a96965ab2a3910a9231f1844ef444457f2
Srinidhi Raghavendran <srirag@cisco.com>
2024-07-11 16:04:11 -0700

    change image path again

--- st_app/pages/Launch_Your_Server.py
+++ st_app/pages/Launch_Your_Server.py
@@ -6,7 +6,7 @@ import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 
-cisco_logo = "../../cisco2.png"
+cisco_logo = "cisco2.png"
 
 with st.sidebar:
     st.image(cisco_logo, use_column_width=True)
@@ -14,7 +14,7 @@ with st.sidebar:
 names = ["Dodge Demon", "Jeep Trackhawk"]
 usernames = ["dodge", "jeep"]
 
-with open('../config.yml') as file:
+with open('config.yml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 authenticator = stauth.Authenticate(
     config['credentials'],
